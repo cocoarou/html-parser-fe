06418c4446a6a7cae140973d15616c1acf1b22e5
l1l-elementaryVM <manuel.pulcinelli.wow@gmail.com>
2022-12-15 11:30:23 +0100

    added routing to back-end controller and view representation of data

--- routes/spells.js
+++ routes/spells.js
@@ -1,9 +1,25 @@
+const { default: axios } = require('axios');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  axios.get("http://localhost:8080/spells").then(response => {
+    var container = response.data.spells;
+    var spells = container.spells;
+    var spellsId = container.spellsId;
+    res.render('spells', {title: 'Spells', spells: spells, spellsId: spellsId});
+  }).catch(err => console.log(err));
+  
 });
+
+router.get('/:name', function(req, res, next) {
+  var name = req.params.name;
+  axios.get("http://localhost:8080/spells/" + name).then(response => {
+    var spell = response.data.spell;
+    res.render('spell', {title: 'Spell', spell: spell});
+  });
+})
 
 module.exports = router;
