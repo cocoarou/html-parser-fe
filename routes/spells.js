73ce2a4a680b423ef835600a24f7d56475c0a1aa
pulcinelli <m.pulcinelli@westpole.it>
2023-07-14 11:25:15 +0200

    cambiata versione di axios; cambiato ip con ipv4 macchina

--- routes/spells.js
+++ routes/spells.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
-  axios.get("http://localhost:8080/spells").then(response => {
+  axios.get("http://192.168.192.1:8080/spells").then(response => {
     var container = response.data.spells;
     var spells = container.spells;
     var spellsId = container.spellsId;
@@ -17,7 +17,7 @@ router.get('/', function(req, res, next) {
 router.get('/:name', function(req, res, next) {
   console.log(`req: ${JSON.stringify(req.params)}`);
   var name = req.params.name;
-  axios.get("http://localhost:8080/spells/" + name).then(response => {
+  axios.get("http://192.168.192.1:8080/spells/" + name).then(response => {
     var spell = response.data.spell;
     res.render('spell', {title: spell.originalName, spell: spell});
   });
